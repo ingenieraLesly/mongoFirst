c19b2994cbcdb50b63410b5cc64f6a4023274908
ingenieralesly <ingenieralesly@gmail.com>
2022-03-06 17:47:31 -0500

    Register working and understood

--- frontend/src/app/home/register/register.component.ts
+++ frontend/src/app/home/register/register.component.ts
@@ -13,14 +13,14 @@ import {
   styleUrls: ['./register.component.css'],
 })
 export class RegisterComponent implements OnInit {
-  registerData: any;
+  registerData: any;//espacio para las vbles globales
   message: string = '';
   horizontalPosition: MatSnackBarHorizontalPosition = 'end';
   verticalPosition: MatSnackBarVerticalPosition = 'top';
   durationInSeconds: number = 2000;
 
   constructor(
-    private _userService: UserService,
+    private _userService: UserService,//vble que consume  lo que llame como UserService (register, login,)
     private _router: Router,
     private _snackBar: MatSnackBar
   ) {
@@ -28,10 +28,46 @@ export class RegisterComponent implements OnInit {
   }
 
   registerUser() {
-    
+    if (
+      !this.registerData.name ||
+      !this.registerData.email ||
+      !this.registerData.password
+    ) {
+      this.message = 'Incomplete data';
+      this.openSnackBarError();
+    } else {
+      this._userService.registerUser(this.registerData).subscribe({
+        next: (v) => {//si todo sale bien (do puntos: esto va a llevar.tipo de vble. en este caso dice que esto es una validación con json adentro)
+          localStorage.setItem('token', v.token);//guardeme en el local un token
+          this._router.navigate(['/saveTask']);//y redireccioneme a saveTask
+          this.message = 'Suscesfull user registration';//y muestreme un mensaje = asigna un valor
+          this.openSnackBarSuccesfull();
+        },
+        error: (e) => {
+          this.message = e.error.message;
+          this.openSnackBarError();
+        },
+      });
+    }
   }
 
-  openSnackBarError() {}
+  openSnackBarSuccesfull() {
+    this._snackBar.open(this.message, 'X', {
+      horizontalPosition: this.horizontalPosition,
+      verticalPosition: this.verticalPosition,
+      duration: this.durationInSeconds,
+      panelClass: ['styleSnackBarSuccesfull'],
+    });
+  }
+
+  openSnackBarError() {
+    this._snackBar.open(this.message, 'X', {
+      horizontalPosition: this.horizontalPosition,
+      verticalPosition: this.verticalPosition,
+      duration: this.durationInSeconds,
+      panelClass: ['styleSnackBarError'],
+    });
+  }
 
   ngOnInit(): void {}
 }
--- frontend/src/app/services/user.service.ts
+++ frontend/src/app/services/user.service.ts
@@ -1,9 +1,20 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { environment } from '../../environments/environment'; //nombre con el que queda exportado es en mnúscula porque es un vble (No una class) y así está escrita en environment.ts
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class UserService {
+  private env: string;
 
-  constructor() { }
+  constructor(private _http: HttpClient) {
+    this.env = environment.APP_URL;
+  }
+  registerUser(user: any) {
+    return this._http.post<any>(this.env + 'user/register', user);
+  }
+  login(user: any) {
+    return this._http.post<any>(this.env + 'user/login', user);
+  }
 }
